59eafe1ab47c37705f0527af4b997606b73e5382
42IceLollies <cct2223@gmail.com>
2023-02-09 19:05:21 -0800

    Made lasso squiggly

--- classes.js
+++ classes.js
@@ -74,7 +74,8 @@ class Physics{
 
 //==================LASSO CLASS============================
 class Lasso{
-    static lassoPoints = [];
+    static lassoPoints;
+    static guidePoints;
     static lassoCounter = 0;
 
     //copy of lasso's variables because it's not working for some reason?
@@ -142,7 +143,7 @@ class Lasso{
 	{
         //console.log(this.lassoCounter);
 		this.lassoCounter++;
-		if(this.lassoCounter == 3)//this line will need to be changed as more of the lasso throw is implemented
+		if(this.lassoCounter == 4)//this line will need to be changed as more of the lasso throw is implemented
 		{
 			this.lassoCounter = 0;
 		}
@@ -165,9 +166,9 @@ class Lasso{
 				Lasso.throwLasso(ctx);
 			break;
 
-			// case 3: 
-			// 	lassoStage = "falling";
-			// break;
+			 case 3: 
+				Lasso.drawLassoFall(ctx);
+			 break;
 
 			// case 4:
 			// 	lassoStage = "grabbing";
@@ -207,35 +208,90 @@ class Lasso{
     //called when spacebar is pressed
     static throwLasso(ctx)
     {
-        const tempLineWidth = ctx.lineWidth;
-        ctx.lineWidth = 5;
-        ctx.strokeStyle =  player.fillColor;
+        this.lassoPoints = [];
+        this.guidePoints = [];
 
-        let lassoPath = new Path2D();
-
+        //divides paths into segments to create curve
         var segments = 10;
         var incrementX = (this.forceX-player.shape.x)/segments;
         var incrementY = (this.forceY-player.shape.y)/segments;
         var energyLoss = 0.85;
-          var currPoint = new Point(player.shape.x, player.shape.y);
+        var currPoint = new Point(player.shape.x, player.shape.y);
+        var squiggliness = 1;
 
-        lassoPath.moveTo(currPoint.x, currPoint.y);
-
-        for(var i = 0; i<segments; i++)
+     
+        //iterates through points to draw line with gradually decreasing slope
+        for(var i = 0; i<segments*2; i+=2)
         {
-            this.lassoPoints.push(currPoint);
+            this.lassoPoints[i] = currPoint;
+            this.guidePoints.push(currPoint);
             currPoint = new Point(currPoint.x+incrementX, currPoint.y+incrementY);
             incrementY *= energyLoss;
-            lassoPath.lineTo(currPoint.x, currPoint.y);
-            ctx.stroke(lassoPath);
+        }
+
+        incrementY = (this.forceY-player.shape.y)/segments;
+
+        //adds extra points to lassoPath to make squiggles
+        for(var i = 1; i<segments*2-1; i+=2)
+        {
+            var xTemp = player.shape.x + incrementX*i + (Math.random()*incrementX);
+            var yTemp = Math.random()>0.5?incrementY + Math.random()*squiggliness : incrementY - Math.random()*squiggliness;
+            yTemp += player.shape.y;
+            incrementY *= energyLoss;
+
+            this.lassoPoints[i] = new Point(xTemp, yTemp);
         }
 
         //lassoPath.smooth(); //maybe ctx.lineCap = 'smooth' - can figure out later;
         //all my prayers that this acually does something
+     
+        this.incrementLassoStage();
+    }
 
+    //notes to myself on what I'm planning to do lol
+//    //make random points along to make line squiggly
+        //make points sink and the starting draw point for line gets set to x of wherever the line has just touched the ground 
+        //use an invisible line without wiggles to determine point of x
+        //everything before x becomes just a straight line on ground
+        //stops at x of the end of the squiggly line
+
+        //need to clear the arrays of points when making a new line?
+        //yeah, at least the guidePoints one, it's being weird
+
+
+    static drawLassoFall(ctx)
+    {
+         //settings for string style
+         const tempLineWidth = ctx.lineWidth;
+         ctx.lineWidth = 5;
+         ctx.strokeStyle =  player.fillColor;
+
+         //path for lasso and path for controlling lasso fall
+         let lassoPath = new Path2D();
+         let guidePath = new Path2D();
+
+         lassoPath.moveTo(this.lassoPoints[0].x, this.lassoPoints[0].y);
+         guidePath.moveTo(this.lassoPoints[0].x, this.lassoPoints[0].y);
+ 
+       for(var i = 1; i<this.lassoPoints.length; i++)
+       {
+          //some typeerror in this line 
+        //console.log(this.lassoPoints[i]);
+         lassoPath.lineTo(this.lassoPoints[i].x, this.lassoPoints[i].y);
+
+         if(i<this.guidePoints.length)
+         {
+            guidePath.lineTo(this.guidePoints[i].x, this.guidePoints[i].y);
+         }
+
+         ctx.stroke(lassoPath);
+         ctx.stroke(guidePath);
+    }
+    
         ctx.lineWidth = tempLineWidth;
         ctx.strokeStyle = "#000000";
     }
+    
 
     static pullInLasso()
     {
