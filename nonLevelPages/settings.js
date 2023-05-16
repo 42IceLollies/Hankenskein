da4c07f9d0e8503e14f7b69496cdcdf0d6c9d10d
42IceLollies <cct2223@gmail.com>
2023-05-16 08:51:40 -0700

    Got homescreen save working, started on color indicator

--- nonLevelPages/settings.js
+++ nonLevelPages/settings.js
@@ -1,24 +1,62 @@
 //bolds the box in settings which shows whether music and sfx are on or off
 function bold(sound)
 {
-    if(game.music==true && (sound=="music"|| sound=="all") && window.location.href.includes("/levels/level"))
+    //if it's only where the href includes levels, you can't see selections on the main menu
+    if(game.music==true && (sound=="music"|| sound=="all") )//&& window.location.href.includes("/levels/level")
     {
         document.getElementById("musicOn").classList.add("bold"); 
         document.getElementById("musicOff").classList.remove("bold");  
-    } else if(game.music== false && (sound=="music"|| sound=="all") && window.location.href.includes("/levels/level"))
+    } else if(game.music== false && (sound=="music"|| sound=="all"))// && window.location.href.includes("/levels/level"))
     {
         document.getElementById("musicOff").classList.add("bold");
         document.getElementById("musicOn").classList.remove("bold");
     }
                 
-    if(game.sfx == true && (sound=="sfx"|| sound=="all") && window.location.href.includes("/levels/level"))
+    if(game.sfx == true && (sound=="sfx"|| sound=="all"))// && window.location.href.includes("/levels/level"))
     {
         document.getElementById("sfxOn").classList.add("bold");
         document.getElementById("sfxOff").classList.remove("bold");
-    } else if(game.sfx == false && (sound=="sfx"|| sound=="all") && window.location.href.includes("/levels/level"))
+    } else if(game.sfx == false && (sound=="sfx"|| sound=="all"))// && window.location.href.includes("/levels/level"))
     {
         document.getElementById("sfxOff").classList.add("bold");
         document.getElementById("sfxOn").classList.remove("bold");
+    }
+
+    if(sound=="color" || sound=="all"){
+            // document.getElementsByClassName("selectX").forEach(()=>this.classList.add("hidden"));
+            // for each loop on all selectX objects
+
+        // switch(game.color)
+        // {
+        //     case "grey":
+        //         document.getElementById("greyX").classList.remove("hidden");
+        //     break;
+
+        //     case "blue": 
+        //         document.getElementById("blueX").classList.remove("hidden");
+        //     break;
+
+        //     case "purple": 
+        //         document.getElementById("purpleX").classList.remove("hidden");
+        //     break;
+
+        //     case "red":
+        //         document.getElementById("redX").classList.remove("hidden");
+        //     break;
+
+        //     case "green":
+        //         document.getElementById("greenX").classList.remove("hidden");
+        //     break;
+
+        //     case "orange":
+        //         document.getElementById("orangeX").classList.remove("hidden");
+        //     break;
+
+        //     case "pink":
+        //         document.getElementById("pinkX").classList.remove("hidden");
+        //     break;
+
+        // }
     }
 }
 
